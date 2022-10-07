565ebb72be5d0f6bcc541e7dd40b22668c8cfe10
Hugo Frizto <frizto@gmail.com>
2022-10-07 14:53:08 -0300

    TimeConversion in C# proj add

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the time to be converted: ");
+            string oldDate = Console.ReadLine();
+            DateTime newDate = DateTime.Parse(oldDate);
+            Console.WriteLine(newDate.ToString("HH:mm:ss"));
+        }
+    }
+}
