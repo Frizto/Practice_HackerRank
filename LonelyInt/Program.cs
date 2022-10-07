a872926f3c105b53e2ab2c06ab6f0d0b7db9a636
Hugo Frizto <frizto@gmail.com>
2022-10-07 15:55:22 -0300

    LonelyInt in C# proj add

---
+++ LonelyInt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LonelyInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write an Array to find the lonely Int number inside it: ");
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int res = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                res = res ^ a[i];
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
