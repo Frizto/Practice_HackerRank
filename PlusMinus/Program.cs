9c2a6dc85d424356fc1edbddbf2d989dc4932b44
Hugo Frizto <frizto@gmail.com>
2022-10-07 14:39:23 -0300

    PlusMinus in C# proj -update-

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace PlusMinus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the Array: ");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Enter the positive, negative and zeros inside the Array separating them with Space: ");
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.plusMinus(arr);
+        }
+    }
+}
---
+++ PlusMinus/Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlusMinus
+{
+    class Result
+    {
+        public static void plusMinus(List<int> arr)
+        {
+            double pos = 0.0;
+            double neg = 0.0;
+            double zer = 0.0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    ++pos;
+                }
+                else if (arr[i] < 0)
+                {
+                    ++neg;
+                }
+                else
+                {
+                    ++zer;
+                }
+            }
+
+            Console.WriteLine("{0:N6}", pos / arr.Count);
+            Console.WriteLine("{0:N6}", neg / arr.Count);
+            Console.WriteLine("{0:N6}", zer / arr.Count);
+        }
+    }
+}
