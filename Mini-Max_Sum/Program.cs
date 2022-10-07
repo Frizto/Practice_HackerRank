0a8a3b51ac07f4f5b0dc9c26233f3d1f1837d0b2
Hugo Frizto <frizto@gmail.com>
2022-10-07 14:31:54 -0300

    Mini-Max_Sum in C# proj

---
+++ Mini-Max_Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Array of numbers separate them with a space: ");
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+        }
+    }
+}
