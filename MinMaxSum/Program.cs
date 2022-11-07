9c55dc46cabea9b021e64efe0b3bb92aa438ba4e
Frizto <frizto@gmail.com>
2022-11-07 19:27:19 -0300

    Re-add MinMaxSum

---
+++ MinMaxSum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MinMaxSum
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            int min = arr.Min();
+            int max = arr.Max();
+            long sum = 0L;
+
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                sum = sum + arr[i];
+            }
+
+            Console.WriteLine(Convert.ToString((sum - max) + " " + (sum - min)));
+        }
+
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+        }
+    }
+}
