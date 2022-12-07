47b485620e38834a2511edc6d06c6cbe77c31a66
Frizto <frizto@gmail.com>
2022-12-07 14:36:57 -0300

    add AdjacentElementsProduct

---
+++ AdjacentElementsProduct/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AdjacentElementsProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an Array: ");
+            string[] input = Console.ReadLine().Split(' ', ',', '.');
+            int[] inputArray = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                inputArray.SetValue(int.Parse(input[i]), i);
+            }
+
+            //DEBUG
+            /*foreach (var item in inputArray)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            int Solution(int[] inputArray)
+            {
+                return inputArray.Select((i, j) => j > 0 ? i * inputArray[j - 1] : int.MinValue).Max();
+            }
+
+            Console.WriteLine(Solution(inputArray));
+        }
+    }
+}
